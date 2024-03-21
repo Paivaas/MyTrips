ff455f07520f5a03b68a0ccea262584db27444ae
Julia Paiva <123731976+Paivaas@users.noreply.github.com>
2024-03-21 17:26:34 -0300

    Page 3

--- app/src/main/java/br/senai/sp/jandira/mytrips/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/mytrips/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -20,6 +21,7 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -28,6 +30,8 @@ import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
@@ -36,17 +40,20 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -68,12 +75,15 @@ class MainActivity : ComponentActivity() {
                     Column {
                         Login()
                         SignUp()
+                        Home()
                     }
                 }
             }
         }
     }
 }
+
+
 
 @Composable
 fun Login() {
@@ -491,9 +501,134 @@ fun SignUp() {
             )
         ){}
     }
-
-
 }
+
+@Composable
+fun Home(){
+
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(0xDCD5D7DA)),
+    ){
+
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .height(220.dp)
+            .background(color = Color(0xFFCE7DC6)),
+        ){
+
+            Column(modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(120.dp),
+                    horizontalAlignment = Alignment.End
+                ){
+                    Box(modifier = Modifier
+                        .width(60.dp)
+                        .height(60.dp)){
+
+                    }
+                    Text(text = "Susanna Hoffs",
+                        color = Color.White,)
+                }
+
+                Column (modifier = Modifier
+                ){
+                    Row {
+                        Icon(
+                            imageVector = Icons.Default.Place,
+                            contentDescription = "Buscar",
+                            tint = Color.White
+                        )
+                        Text(text = "You're in Paris",
+                            color = Color.White
+                        )
+                    }
+                    Text(text = "My Trips",
+                        color = Color.White,
+                        fontSize = 34.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
+        }
+        Row (modifier = Modifier
+            .padding(16.dp)
+        ) {
+            Text(text = "Categories")
+        }
+        LazyRow(modifier = Modifier
+            .padding(start = 16.dp)
+
+        ){
+            items(10){
+                Card (modifier = Modifier
+                    .width(102.dp)
+                    .height(64.dp)
+                    .padding(end = 8.dp),
+                    colors = CardDefaults.cardColors(containerColor = Color(0xFFCF06F0)),){
+                    Column (
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ){
+                        Icon(
+                            imageVector = Icons.Default.Place,
+                            contentDescription = "Place",
+                            tint = Color.White
+                        )
+                        Text(text = "Montain",
+                            color = Color.White
+                        )
+                    }
+                }
+            }
+        }
+
+        Column (modifier = Modifier.padding(16.dp)){
+
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                modifier = Modifier
+                    .fillMaxWidth(),
+                placeholder = {
+                    Text(
+                        text = "Buscar personagens",
+                        color = Color.Gray
+                    )
+                },
+                shape = RoundedCornerShape(12.dp),
+                colors = TextFieldDefaults.colors(Color.White),
+                trailingIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = "Search your destiny",
+                            tint = Color.Gray
+                        )
+                    }
+                }
+            )
+        }
+
+
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun HomePreview() {
+    MyTripsTheme {
+        Home()
+    }
+}
+
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
